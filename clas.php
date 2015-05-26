b378c1fadf15b17b51703b190f6558a8f47d1880
KlimovSN <klimov.sn@bk.ru>
2015-05-26 17:32:21 +0500

    Лабораторная работа №1
    Создан сайт средствами HTML5. Сайт посвящен игре жанра MMORPG "Star Wars
    The Old Republic", на данный момент на нем присутствуют 2 страницы, одна
    с небольшим описание игры, вторая с демонстрацией классов в игре. В
    дальнейшем будет добавлена форма для покупки внутреигровой валюты

---
+++ clas.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="author" content="Apik" />
+
+	<title>Star Wars The Old Republic</title>
+    
+     <link rel="stylesheet" type="text/css" href="style.css" />
+        
+</head>
+<body>
+
+<header>
+</header>
+
+<div class="main">
+
+<nav>
+        <a href="index.php">Главная</a>  <a href="clas.php">Классы</a> <a href="#">Покупка валюты</a>  <a href="#">Регистрация</a>
+</nav>
+
+<div class="content">
+
+<div class="warning"> Классы Star Wars: The Old Republic! </div>
+  
+  <br /><br />
+  <div class="all_game_bg">
+  
+  <div class="game_bg">
+    <a href="#"><img src="img/Десантник.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Рыцарь.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Контрабандист.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Консул.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Охотник.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Агент.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Воин.png" alt="" /></a>
+  </div>
+  <div class="game_bg">
+    <a href="#"><img src="img/Инквизитор.png" alt="" /></a>
+  </div>
+
+  </div>
+  
+  <article>
+  </article>
+  
+</div>
+
+</div>
+
+<footer>
+
+</footer>
+
+
+</body>
+</html>
