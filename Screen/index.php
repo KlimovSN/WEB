1c36738bb9f682007910def5a7d04bd24f04f424
KlimovSN <klimov.sn@bk.ru>
2015-05-31 20:01:54 +0500

    Лабораторная Работа #6
    ScreenShot операционной системы. С помощью html и CSS была создана
    страница с изображение скриншота из Windows 8

---
+++ Screen/index.php
@@ -0,0 +1,141 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Apik" />
+
+	<title>Windows 8</title>
+    
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+<div class ="content">
+
+<div id = "back">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+width="30px" height="30px" viewBox="0 0 30 30" enable-background="new 0 0 30 30" xml:space="preserve">
+<circle fill="#FFFFFF" cx="15.046" cy="14.921" r="14.984"/>
+<circle fill="#19046A" cx="15.046" cy="14.921" r="12.26"/>
+<rect x="8.299" y="13.104" fill="#FFFFFF" width="13.817" height="3.634"/>
+<rect x="5.455" y="10.093" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -4.9318 11.6453)" fill="#FFFFFF" width="12.269" height="3.364"/>
+<rect x="10.862" y="5.9" fill="#19046A" width="7.135" height="2.697"/>
+<rect x="5.454" y="16.384" transform="matrix(0.7071 0.7071 -0.7071 0.7071 16.1686 -2.9032)" fill="#FFFFFF" width="12.27" height="3.364"/>
+<rect x="10.748" y="21.191" fill="#19046A" width="7.395" height="2.698"/>
+</svg>
+</div>
+
+<div id = "clear">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+width="15px" height="15px" viewBox="0 0 15 15" enable-background="new 0 0 15 15" xml:space="preserve">
+<rect x="1.784" y="2.145" fill="#070707" width="11.319" height="11.23"/>
+<rect x="11.144" y="2.126" transform="matrix(-0.707 0.7072 -0.7072 -0.707 34.1592 1.3318)" fill="#FFFFFF" width="11.319" height="11.232"/>
+<rect x="1.804" y="-7.208" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 11.6157 -7.9955)" fill="#FFFFFF" width="11.319" height="11.231"/>
+<rect x="1.756" y="11.466" transform="matrix(-0.707 0.7072 -0.7072 -0.707 24.7366 23.9145)" fill="#FFFFFF" width="11.318" height="11.231"/>
+<rect x="-7.604" y="2.151" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 2.1701 14.6324)" fill="#FFFFFF" width="11.318" height="11.229"/>
+</svg>
+</div>
+
+<div id = "down">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+width="15px" height="15px" viewBox="0 0 15 15" enable-background="new 0 0 15 15" xml:space="preserve">
+<rect x="1.73" y="2.114" fill="#070707" width="11.5" height="11.411"/>
+<rect x="3.256" y="5.402" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 31.213 9.2857)" fill="#D1CCE0" width="20.854" height="11.411"/>
+<rect x="1.75" y="-7.388" transform="matrix(-0.7072 0.707 -0.707 -0.7072 11.6132 -8.1774)" fill="#D1CCE0" width="11.5" height="11.409"/>
+<rect x="-4.889" y="0.91" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 9.0668 17.7324)" fill="#D1CCE0" width="11.499" height="19.668"/>
+</svg></div>
+
+	<div class = "strings">
+	
+	<div id = "title"> Настройка учетной записи Майкрософт</div>
+		
+		<div > 
+			Вы можете использовать свою учетную запись Майкрософт для Входа в Xbox LIVE, Windows <br>Phone и другие службы Майкрософт. 
+		</div>
+		<br>
+		<table>
+		
+		<tr>
+		<td width="130px"  class = "string">
+		Адрес электронной <br>почты
+		</td>
+		<td class ="tb" width="300px">
+		<span> admin1973@mail.ru</<span>
+		</td>
+		</tr>
+		<tr>
+		<td width="130px"  class = "string">
+		Новый пароль
+		</td>
+		<td class ="tb" width="300px" >
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		</td>
+		</tr>
+		<tr>
+		<td width="130px"  class = "string">
+		Подтверждение
+		</td>
+		<td class ="tb" width="300px">
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		<div id = "circle"> </div>
+		</td>
+		</tr>
+		<tr>
+		<td width="130px" class = "string">
+		Фамилия
+		</td>
+		<td class ="tbu" width="300px">
+		<span> Донской</<span>
+		</td>
+		</tr>
+		<tr>
+		<td width="130px" class = "string">
+		Имя
+		</td>
+		<td class ="tb" width="300px">
+		<span> Дмитрий</<span>
+		</td>
+		</tr>
+		<tr>
+		<td width="130px" class = "string">
+		Страна или регион
+		</td>
+		<td class ="tb" width="300px">
+		<span> Россия</<span>
+		</td>
+		</tr>
+		</table>
+		
+		
+
+		
+	</div>
+	
+	<br><br><br><br><br>
+	<div id = "buts">
+	<div class = "but1"> <span id="under">И</span>зменить метод ввода</div>
+	<div class = "but2"> <span id="under">О</span>тмена</div>
+	<div class = "but3"> Д<span id="under">а</span>лее</div>
+	
+	</div>
+
+</div>
+</body>
